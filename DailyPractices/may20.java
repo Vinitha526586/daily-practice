381513ca887c488589bebbcedeeb689a9311c079
Vinitha <vinitha901@gmail.com>
2026-05-21 07:55:15 +0530

    Updated may20.java code

---
+++ DailyPractices/may20.java
@@ -0,0 +1,176 @@
+package DailyPractices;
+
+public class may20 {
+    public static void main (String[] args){
+       
+       /*
+        String name = "Vini";
+        int age = 12;
+        String course = "B.E";
+        char grade = 'A';
+        double CGPA = 8.13;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(course);
+        System.out.println(grade);
+        System.out.println(CGPA);
+        */
+
+       /* area of rectaangular
+       double length = 89.6;
+       double width = 67.8;
+       double result = length * width;
+       System.out.println(result);
+       */
+       /*Simple interest
+        int principal = 20000;
+        int interest = 1000;
+        int time = 3;
+        int simpleInterest;
+        simpleInterest = (principal * interest * time) / 100;
+        System.out.println("simpleInterest =" + simpleInterest);
+        */
+       /*Even or Odd
+       int num = 21;
+       if(num % 2 == 0){
+        System.out.println("Even");
+       }
+       else{
+        System.out.println("Odd");
+       }
+       */
+      /*positive or negative
+      int n = 0;
+      if(n > 0){
+        System.out.println("Positive");
+      }
+      else if(n < 0){
+        System.out.println("Negative");
+      }
+      else{
+        System.out.println("Zero");
+      }
+      */
+     /*Greatest Number
+     int a = 44;
+     int b = 43;
+     int c = 78;
+     if(a>b && a>c){
+        System.out.println("a is greater");
+     }
+     else if(b>a && b>c){
+        System.out.println("b is greater");
+     }
+     else{
+        System.out.println("c is greater");
+     }
+     */
+    /* swap two numbers
+    int d = 34;
+    int e = 45;
+    int temp;
+    temp = d;
+    d = e;
+    e = temp;
+    System.out.println("After Swapping");
+    System.out.println("d:" + d);
+    System.out.println("e:" + e);
+    */
+   /* vote eligibility
+   int age = 18;
+   if(age >= 18){
+    System.out.println("Eligible");
+   }
+   else{
+    System.out.println("Not eligible");
+   }
+   */  
+
+  /* grade system
+  int mark = 78;
+  if(mark >90){
+    System.out.println("A grade");
+  }
+  else if(mark >75){
+    System.out.println("B grade");
+  }
+  else if(mark >= 50){
+    System.out.println("c grade");
+  }
+  else{
+    System.out.println("Fail");
+  }*/
+
+ /*Calculator using switch
+ int a = 23;
+ int b = 48;
+ char operator = '-';
+switch(operator){
+ case '+':
+    System.out.println(a+b);
+    break;
+ case '-':
+    System.out.println(a-b);
+    break;
+ case '*':
+    System.out.println("a*b");
+    break;
+ case '/':
+    System.out.println("a/b");
+    break;
+ case '%':
+    System.out.println("a%b");
+    break;
+ default:
+    System.out.println("Invalid Operator");
+}
+*/
+
+ /*Print 1–100
+ for(int i=1;i<=100;i++){
+    System.out.println(i);
+ }
+ */ 
+/*Sum of numbers
+int sum = 0;
+for(int i=1;i<=20;i++){
+    sum += i;
+}
+System.out.println(sum);
+*/
+
+/* sum of digit
+int num = 123;
+int sum = 0;
+while(num != 0){
+    sum += num % 10;
+    num /= 10;
+}
+System.out.println(sum);
+*/
+
+/*Multiplication table
+int num = 6;
+for(int i=1;i<=10;i++){
+    System.out.println(num + "x" + i + "=" + (num*i));
+}
+*/
+
+//Reverse numbers
+int num = 145678;
+int rev = 0;
+while(num != 0){
+    int digit = num % 10;
+    rev = rev * 10 + digit;
+    num /= 10;
+}
+System.out.println(rev);
+
+
+
+
+
+    }
+}
+
